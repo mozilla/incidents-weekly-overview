38bb516b9d62d748e7f2856870b5679ca6820545
Will Kahn-Greene <willkg@mozilla.com>
2026-04-03 17:58:12 -0400

    fix(libreportparser): remove anchor links from summary

--- src/iim/libreportparser.py
+++ src/iim/libreportparser.py
@@ -13,6 +13,7 @@ from iim.libreport import ActionItem, IncidentReport, normalize_entities
 
 
 DATE_RE = re.compile(r"(\d{4}-\d{2}-\d{2})")
+ANCHOR_RE = re.compile(r"\s*\{#[a-zA-Z0-9.%\-'\"():]+\}")
 DATETIME_RE = re.compile(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2})")
 ISO_DATETIME_RE = re.compile(r"(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2})")
 JIRA_ISSUE_RE = re.compile(r"(IIM\-\d+)")
@@ -91,7 +92,7 @@ def get_text(token, keep_links: bool = True):
             marko.inline.RawText,
         ),
     ):
-        text.append(token.children)
+        text.append(ANCHOR_RE.sub("", token.children))
     elif isinstance(token, marko.inline.Link):
         link_text = []
         for child in token.children:
