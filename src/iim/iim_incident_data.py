67cc1d2b4e2ed081390dcb94735f149f599d70dd
Will Kahn-Greene <willkg@mozilla.com>
2026-05-01 12:49:21 -0400

    fix(iim-weekly-overview): change "age" to "impact duration"
    Age was the delta from impact_start to resolution. That's not very
    meaningful. We really want to know what the duration of impact was.
    Duration of impact starts at the earliest of (impact_start, declared, or
    alerted) and ends with mitigation.
    
    This changes Age to Impact Duration in the report.

--- src/iim/iim_incident_data.py
+++ src/iim/iim_incident_data.py
@@ -48,7 +48,7 @@ def iim_incident_data(issue_key, client_secret_file):
     # Include computed properties that dataclasses.asdict() skips
     data["entity_bucket"] = report.entity_bucket
     data["is_completed"] = report.is_completed
-    data["age"] = fmt_td(report.age)
+    data["impact_duration"] = fmt_td(report.impact_duration)
     data["tt_declared"] = fmt_td(report.tt_declared)
     data["tt_alerted"] = fmt_td(report.tt_alerted)
     data["tt_responded"] = fmt_td(report.tt_responded)
